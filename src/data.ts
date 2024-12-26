34f3fe52f04758cc7f826692d180dabd3f03d8e8
Marco Accorinti <accosk8@gmail.com>
2024-12-26 15:51:42 +0100

    refactor: update colors in todo items for better visual distinction

--- src/data.ts
+++ src/data.ts
@@ -6,12 +6,12 @@ let todos: Todo[] = [
     { date: new Date("2021-10-10T15:04:45"), id: "3", color: "magenta", items: [ {id: "1", content: "Lorem ipsum"}, {id: "2", content: "Lorem ipsum"} ] },
     { date: new Date("2021-10-10T12:01:11"), id: "4", color: "cyan", items: [ {id: "1", content: "Lorem ipsum"}, {id: "2", content: "Lorem ipsum"} ] },
     { date: new Date("2021-10-10T14:01:12"), id: "5", color: "magenta", items: [ {id: "1", content: "Lorem ipsum"}, {id: "2", content: "Lorem ipsum"}, {id: "3", content: "Lorem ipsum"}, {id: "4", content: "Lorem ipsum"} ] },
-    { date: new Date("2021-10-10T15:04:45"), id: "6", color: "magenta", items: [ {id: "1", content: "Lorem ipsum Lorem ipsum"}, {id: "2", content: "Lorem ipsum"} ] },
-    { date: new Date("2021-10-10T12:01:11"), id: "7", color: "magenta", items: [ {id: "1", content: "Lorem ipsum"}, {id: "2", content: "Lorem ipsum"} ] },
-    { date: new Date("2021-10-10T14:01:12"), id: "8", color: "magenta", items: [ {id: "1", content: "Lorem ipsum"}, {id: "2", content: "Lorem ipsum"} ] },
-    { date: new Date("2021-10-10T15:04:45"), id: "9", color: "magenta", items: [ {id: "1", content: "Lorem ipsum"}, {id: "2", content: "Lorem ipsum"} ] },
+    { date: new Date("2021-10-10T15:04:45"), id: "6", color: "orange", items: [ {id: "1", content: "Lorem ipsum Lorem ipsum"}, {id: "2", content: "Lorem ipsum"} ] },
+    { date: new Date("2021-10-10T12:01:11"), id: "7", color: "black", items: [ {id: "1", content: "Lorem ipsum"}, {id: "2", content: "Lorem ipsum"} ] },
+    { date: new Date("2021-10-10T14:01:12"), id: "8", color: "grey", items: [ {id: "1", content: "Lorem ipsum"}, {id: "2", content: "Lorem ipsum"} ] },
+    { date: new Date("2021-10-10T15:04:45"), id: "9", color: "white", items: [ {id: "1", content: "Lorem ipsum"}, {id: "2", content: "Lorem ipsum"} ] },
     { date: new Date("2021-10-10T12:01:11"), id: "10", color: "magenta", items: [ {id: "1", content: "Lorem ipsum"}, {id: "2", content: "Lorem ipsum"} ] },
-    { date: new Date("2021-10-10T14:01:12"), id: "11", color: "magenta", items: [ {id: "1", content: "Lorem ipsum"}, {id: "2", content: "Lorem ipsum"} ] },
+    { date: new Date("2021-10-10T14:01:12"), id: "11", color: "purple", items: [ {id: "1", content: "Lorem ipsum"}, {id: "2", content: "Lorem ipsum"} ] },
     { date: new Date("2021-10-10T15:04:45"), id: "12", color: "magenta", items: [ {id: "1", content: "Lorem ipsum"}, {id: "2", content: "Lorem ipsum"} ] },
 ]
 
