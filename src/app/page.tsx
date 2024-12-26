12fdaaa26ecb88834daebf678689ed8317c629f7
Marco Accorinti <accosk8@gmail.com>
2024-12-26 13:39:00 +0100

    style: add container class to Home component for improved layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { todos } from '../data';
 
 export default function Home() {
   return (
-    <div>
+    <div className='container mx-auto'>
         {todos.map(todo => (
           <TodoComponent {...todo} />
         ))}
