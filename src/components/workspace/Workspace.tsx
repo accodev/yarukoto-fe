c5708afc494a9683b451cdc11c6fcb85674f0b80
Marco Accorinti <accosk8@gmail.com>
2025-01-01 12:29:47 +0100

    feat: add logout button

--- src/components/workspace/Workspace.tsx
+++ src/components/workspace/Workspace.tsx
@@ -1,4 +1,8 @@
 import { Notes } from '@/components/notes/Notes';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import Link from 'next/link';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 
 interface WorkspaceProps {
   workspaceId: string;
@@ -6,7 +10,12 @@ interface WorkspaceProps {
 
 function Workspace({ workspaceId }: WorkspaceProps) {
   return (
-    <Notes workspaceId={workspaceId} />
+    <div className='container mx-auto h-dvh p-5 relative'>
+      <Link href='/' className='absolute top-5 right-5 bg-blue-100 text-blue-700 p-2 rounded hover:bg-blue-200'>
+        <FontAwesomeIcon icon={faSignOutAlt} />
+      </Link>
+      <Notes workspaceId={workspaceId} />
+    </div>
   );
 }
 
